6189e8f2662f768c84b312bf18c619381d76ee6f
ferivb <fe.rivasb@gmail.com>
2022-11-24 19:47:37 -0500

    Added test 2: posting 10 POJOS, Next: Test 3 Get test and no repeated emails assertion

--- src/test/java/tests/Post10POJOsTest.java
+++ src/test/java/tests/Post10POJOsTest.java
@@ -1,5 +1,6 @@
 package tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import util.models.BankTransaction;
 import util.test.BaseTest;
@@ -15,5 +16,23 @@ public class Post10POJOsTest extends BaseTest {
         List<BankTransaction> pojos = generatePOJO(10);
 
         log.info("Generating duplicated POJOs");
+        BankTransaction duplicated1 = new BankTransaction(
+                "duppp", "plicated", 123333, 125553.90, "whatever",
+                "abc@email.com", true, "Peru", "12345666"
+        );
+        pojos.add(duplicated1);
+        BankTransaction duplicated2 = new BankTransaction(
+                "this", "shouldn't post", 122223, 127773.90, "duplication",
+                "abc@email.com", false, "Japan", "12553456"
+        );
+        pojos.add(duplicated2);
+
+        log.info("Removing duplicates");
+        List<BankTransaction> pojosTrimmed = duplicatedEmailTrimmer(pojos);
+
+        log.info("Serializing POJOs and posting to mockAPI");
+        for (BankTransaction i: pojosTrimmed){
+            Assert.assertEquals(postToAPI(pojoSerializer(i)),201);
+        }
     }
 }
